489f87b02d51c05165d8f0d8d4bd5642171be992
vazh2100 <vazh2100@list.ru>
2024-11-22 16:22:18 +0300

    create project

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.7.2" apply false
+        id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+        id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GeoEventApplication"
+include(":app")
+
+
