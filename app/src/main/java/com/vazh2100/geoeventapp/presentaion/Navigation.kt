00b3b468959633f96a0de08ae5c3ca13c1d37a3a
vazh2100 <vazh2100@list.ru>
2024-11-25 20:38:04 +0300

    added animation between screens

--- app/src/main/java/com/vazh2100/geoeventapp/presentaion/Navigation.kt
+++ app/src/main/java/com/vazh2100/geoeventapp/presentaion/Navigation.kt
@@ -1,6 +1,10 @@
 package com.vazh2100.geoeventapp.presentaion
 
 import EventDetailsScreen
+import androidx.compose.animation.*
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,9 +13,49 @@ import com.vazh2100.geoeventapp.domain.entities.Event
 import com.vazh2100.geoeventapp.presentaion.screen.eventList.EventListScreen
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AppNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "eventList") {
+    NavHost(
+        navController = navController,
+        startDestination = "eventList",
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { it }, // Вход справа
+                animationSpec = tween(
+                    durationMillis = 400,
+                    easing = EaseInOut // Сглаживание движения
+                )
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { -it }, // Уход влево
+                animationSpec = tween(
+                    durationMillis = 400,
+                    easing = EaseInOut // Сглаживание движения
+                )
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -it }, // Возврат с левой стороны
+                animationSpec = tween(
+                    durationMillis = 400,
+                    easing = EaseInOut // Сглаживание движения
+                )
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { it }, // Уход вправо
+                animationSpec = tween(
+                    durationMillis = 400,
+                    easing = EaseInOut // Сглаживание движения
+                )
+            )
+        }
+    ) {
         composable("eventList") {
             EventListScreen(navController)
         }
@@ -22,3 +66,4 @@ fun AppNavGraph(navController: NavHostController) {
         }
     }
 }
+
