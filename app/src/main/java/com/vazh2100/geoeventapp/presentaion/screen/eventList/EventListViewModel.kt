d5f24c28908706b44fc3c6fd39782b1a5d654e27
vazh2100 <vazh2100@list.ru>
2024-11-27 20:29:04 +0300

    added event loading with geo-filtering and sorting when geolocation changes from null to non-null and when saved filter has radius or distance sorting

--- app/src/main/java/com/vazh2100/geoeventapp/presentaion/screen/eventList/EventListViewModel.kt
+++ app/src/main/java/com/vazh2100/geoeventapp/presentaion/screen/eventList/EventListViewModel.kt
@@ -2,15 +2,19 @@ package com.vazh2100.geoeventapp.presentaion.screen.eventList
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vazh2100.geoeventapp.domain.entities.GPoint
 import com.vazh2100.geoeventapp.domain.entities.NetworkStatus
 import com.vazh2100.geoeventapp.domain.entities.event.Event
 import com.vazh2100.geoeventapp.domain.entities.event.EventSearchParams
+import com.vazh2100.geoeventapp.domain.entities.event.EventSortType
 import com.vazh2100.geoeventapp.domain.usecase.GetFilteredEventsUseCase
 import com.vazh2100.geoeventapp.domain.usecase.GetLocationStatusUseCase
 import com.vazh2100.geoeventapp.domain.usecase.GetNetworkStatusUseCase
 import com.vazh2100.geoeventapp.domain.usecase.GetSavedFiltersUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.launch
 
 /**
@@ -45,31 +49,32 @@ class EventListViewModel(
 
     // Flow for location status and current user's coordinates
     val locationStatus = getLocationStatusUseCase.locationStatus
-    val userGPoint = getLocationStatusUseCase.userGPoint
+    val userGPoint: StateFlow<GPoint?> = getLocationStatusUseCase.userGPoint
 
     init {
-        loadSavedFilters()
+        viewModelScope.launch {
+            loadSavedFilters()
+            loadEvents()
+            reactOnUserGPChanged()
+        }
     }
 
     /**
      * Loads the saved event filters from the data source.
      */
-    private fun loadSavedFilters() {
-        viewModelScope.launch {
-            _isLoading.value = true
-            val result = getSavedFiltersUseCase.get()
-
-            result.onSuccess { eventFilter ->
-                // Set the loaded filter and fetch the events based on it
-                _searchParams.value = eventFilter
-                loadEvents()
-            }.onFailure { error ->
-                _errorMessage.value = error.message
-                error.printStackTrace()
-            }
-            _isLoading.value = false
+    private suspend fun loadSavedFilters() {
+        _isLoading.value = true
+        val result = getSavedFiltersUseCase.get()
+        result.onSuccess {
+            _searchParams.value = it
+        }.onFailure {
+            _errorMessage.value = it.message
+            it.printStackTrace()
         }
+        _isLoading.value = false
     }
+
+
 
     /**
      * Applies the new event filter and reloads the events if the filter has changed.
@@ -81,31 +86,37 @@ class EventListViewModel(
         val changed = eventSearchParams != _searchParams.value
         _searchParams.value = eventSearchParams
         if (changed) {
-            // Reload events when the filter changes
-            loadEvents()
+            viewModelScope.launch {
+                loadEvents()
+            }
         }
     }
 
     /**
      * Loads the events based on the current filter.
      */
-    private fun loadEvents() {
-        viewModelScope.launch {
-            _isLoading.value = true
-            val result = getFilteredEventsUseCase.get(_searchParams.value)
+    private suspend fun loadEvents() {
+        _isLoading.value = true
+        val result = getFilteredEventsUseCase.get(_searchParams.value)
 
-            result.onSuccess { events ->
-                // Update the events state with the new list of events
-                _events.value = events
-                _errorMessage.value = null
-            }.onFailure { error ->
-                // Handle any errors that occur while loading events
-                _errorMessage.value = error.message
-                error.printStackTrace()
-            }
-
-            _isLoading.value = false
+        result.onSuccess { events ->
+            // Update the events state with the new list of events
+            _events.value = events
+            _errorMessage.value = null
+        }.onFailure { error ->
+            // Handle any errors that occur while loading events
+            _errorMessage.value = error.message
+            error.printStackTrace()
         }
+        _isLoading.value = false
     }
+
+    private fun reactOnUserGPChanged() =
+        userGPoint.scan(userGPoint.value) { old, new ->
+            if (old == null && new != null && (_searchParams.value.radius != null || _searchParams.value.sortType == EventSortType.DISTANCE)) {
+                loadEvents()
+            }
+            new
+        }.launchIn(viewModelScope)
 }
 
