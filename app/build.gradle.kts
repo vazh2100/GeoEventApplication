dfdc77e90df3971ef073ca5731989b269e9f2d2c
vazh2100 <vazh2100@list.ru>
2024-11-29 19:15:43 +0300

    version 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.vazh2100.geoeventapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = getVersionName(versionCode!!)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
