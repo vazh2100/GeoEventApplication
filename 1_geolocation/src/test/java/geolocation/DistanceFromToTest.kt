b1323a8fda1cec4c3ea0aa56543cf0d7c37c8c45
vazh2100 <vazh2100@list.ru>
2025-03-03 18:29:18 +0300

    Refactoring: Tests

--- 1_geolocation/src/test/java/geolocation/DistanceFromToTest.kt
+++ 1_geolocation/src/test/java/geolocation/DistanceFromToTest.kt
@@ -5,44 +5,24 @@ import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
 class DistanceFromToTest {
+    private val cases = listOf(
+        // The same
+        Case(GPoint(52.5200, 13.4050), GPoint(52.5200, 13.4050), 0.0, 0.0),
+        // Moscow - Saint Petersburg
+        Case(GPoint(55.7558, 37.6173), GPoint(59.9343, 30.3351), 633.0, 1.0),
+        // Paris - Los Angeles
+        Case(GPoint(48.8566, 2.3522), GPoint(34.0522, -118.2437), 9085.5, 1.0),
+        // Points on the equator
+        Case(GPoint(0.0, 0.0), GPoint(0.0, 10.0), 1112.0, 1.0)
+    )
 
     @Test
-    fun `when points the same, a distance is 0`() {
-        val point1 = GPoint(52.5200, 13.4050) // Berlin
-        val point2 = GPoint(52.5200, 13.4050) // Berlin
-        val distance = point1.distanceTo(point2)
-        assertEquals(0.0, distance, 0.0) // The distance should be 0, with no margin of error.
+    @Suppress("DestructuringDeclarationWithTooManyEntries")
+    fun `when points are well known, a distance is expected`() {
+        for ((point1, point2, expected, delta) in cases) {
+            assertEquals(expected, point1.distanceTo(point2), delta)
+        }
     }
 
-    @Test
-    fun `when points are well known cities, a distance is expected`() {
-        val moscow = GPoint(55.7558, 37.6173) // Moscow
-        val saintPetersburg = GPoint(59.9343, 30.3351) // Saint Petersburg
-        val expected = 633.0
-        val distance = moscow.distanceTo(saintPetersburg)
-        assertEquals(expected, distance, 1.0) // Allow a margin of error of 1 km.
-    }
-
-    /**
-     * Test case for calculating the distance between two random cities (Paris and Los Angeles).
-     * The expected distance is approximately 9085.5 km.
-     */
-    @Test
-    fun `when points are well known cities 2, a distance is expected`() {
-        val point1 = GPoint(48.8566, 2.3522) // Paris
-        val point2 = GPoint(34.0522, -118.2437) // Los Angeles
-        val expected = 9085.5
-        val distance = point1.distanceTo(point2)
-        // Expected distance: approximately 9085.5 km
-        assertEquals(expected, distance, 1.0) // Allow a margin of error of 50 km.
-    }
-
-    @Test
-    fun `when points belongs to the equator, a distance is expected`() {
-        val point1 = GPoint(0.0, 0.0) // Point on the equator and the Prime Meridian
-        val point2 = GPoint(0.0, 10.0) // Point on the equator, but 10° east
-        val expected = 1112.0
-        val distance = point1.distanceTo(point2)
-        assertEquals(expected, distance, 1.0) // Allow a margin of error of 1 km.
-    }
+    data class Case(val point1: GPoint, val point2: GPoint, val expected: Double, val delta: Double)
 }
--- 2_events/src/test/java/events/EventsProcessorFilterTest.kt
+++ 2_events/src/test/java/events/EventsProcessorFilterTest.kt
@@ -6,154 +6,82 @@ import events.entities.EventType
 import events.entities.EventsProcessor
 import geolocation.entity.GPoint
 import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.assertTrue
 import org.junit.Test
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
-/**
- * Unit tests for the event filtering functionality in the EventsProcessor.
- * This test class focuses on validating the filtering of events based on different criteria:
- * 1. Event type (e.g., CONCERT, SPORTING_EVENT)
- * 2. Radius from the user's location
- * 3. Start date of the event
- * 4. End date of the event
- * 5. Combined filtering by type and radius
- * 6. No filters applied, returning all events.
- */
 internal class EventsProcessorFilterTest {
 
-    // Constants for filtering tests
     private val userGPoint = GPoint(55.0, 37.0)
     private val radius = 65
     private val now = Instant.now()
     private val event1 = Event(
         id = 1,
-        name = "Concert", // Event type CONCERT
-        description = "Description",
+        name = "Concert",
+        description = "",
         type = EventType.CONCERT,
         latitude = 55.0,
         longitude = 37.01,
-        city = "Moscow",
+        city = "",
         date = now.minus(1, ChronoUnit.HOURS)
     )
     private val event2 = Event(
         id = 2,
-        name = "Sport Event", // Event type SPORTING_EVENT
-        description = "Description",
+        name = "Sport Event",
+        description = "",
         type = EventType.SPORTING_EVENT,
         latitude = 56.0,
         longitude = 38.0,
-        city = "Saint-Petersburg",
+        city = "",
         date = now.plus(1, ChronoUnit.HOURS)
     )
     private val event3 = Event(
         id = 3,
-        name = "Festival", // Event type CONCERT
-        description = "Description",
+        name = "",
+        description = "",
         type = EventType.CONCERT,
         latitude = 55.5,
         longitude = 37.5,
-        city = "Moscow",
+        city = "",
         date = now.plus(1, ChronoUnit.DAYS)
     )
     private val event4 = Event(
         id = 4,
-        name = "Football Match", // Event type SPORTING_EVENT
-        description = "Description",
+        name = "",
+        description = "",
         type = EventType.SPORTING_EVENT,
         latitude = 60.0,
         longitude = 40.0,
-        city = "Somewhere",
+        city = "",
         date = now.minus(1, ChronoUnit.DAYS).minus(1, ChronoUnit.HOURS)
     )
 
     // Predefined list of events used in the tests
     private val events = listOf(event1, event2, event3, event4)
 
-    /**
-     * Test the filtering of events by type (CONCERT).
-     * - Filters events of type CONCERT.
-     * - Verifies that the correct number of events (2) of type CONCERT are returned.
-     */
-    @Test
-    fun `test filtering events by type CONCERT`() {
-        val eventSearchParams = EventSearchParams(
-            type = EventType.CONCERT,
-            startDate = null,
-            endDate = null,
-            radius = null
-        )
-        val filteredEvents = EventsProcessor.filter(events, eventSearchParams, null)
+    private val cases = listOf(
+        Case(events, EventSearchParams(type = EventType.CONCERT), listOf(event1, event3)),
+        Case(events, EventSearchParams(radius = radius), listOf(event1, event3)),
+        Case(events, EventSearchParams(startDate = now), listOf(event2, event3)),
+        Case(
+            events,
+            EventSearchParams(EventType.CONCERT, startDate = now, endDate = now.plus(2, ChronoUnit.DAYS), radius),
+            listOf(event3)
+        ),
+        // invalid ranges
+        Case(
+            events,
+            EventSearchParams(startDate = now.plus(1, ChronoUnit.DAYS), endDate = now.minus(1, ChronoUnit.DAYS)),
+            listOf()
+        ),
+    )
 
-        assertEquals(2, filteredEvents.size)
-        assertEquals("Concert", filteredEvents[0].name)
-        assertEquals("Festival", filteredEvents[1].name)
+    @Test
+    fun `when the input and the filter are in place, the result is expected`() {
+        for ((input, filter, expected) in cases) {
+            assertEquals(expected, EventsProcessor.filter(input, filter, userGPoint))
+        }
     }
 
-    /**
-     * Test the filtering of events by radius.
-     * - Filters events within a specified radius from the user's location.
-     * - Verifies that the correct number of events (2) within the radius are returned.
-     */
-    @Test
-    fun `test filtering events by radius`() {
-        val eventSearchParams =
-            EventSearchParams(type = null, startDate = null, endDate = null, radius = radius)
-        val filteredEvents = EventsProcessor.filter(events, eventSearchParams, userGPoint)
-
-        assertEquals(2, filteredEvents.size)
-        assertEquals("Concert", filteredEvents[0].name)
-        assertEquals("Festival", filteredEvents[1].name)
-    }
-
-    /**
-     * Test the filtering of events by start date.
-     * - Filters events that start after the current date.
-     * - Verifies that the correct number of future events (2) are returned.
-     */
-    @Test
-    fun `test filtering events by start date`() {
-        val eventSearchParams =
-            EventSearchParams(startDate = now, endDate = null, radius = null, type = null)
-        val filteredEvents = EventsProcessor.filter(events, eventSearchParams, null)
-
-        assertEquals(2, filteredEvents.size)
-        assertEquals("Sport Event", filteredEvents[0].name)
-        assertEquals("Festival", filteredEvents[1].name)
-    }
-
-    /**
-     * Test filtering events by type, radius, start date, and end date.
-     * - Filters events of type CONCERT within radius and after the current date.
-     * - Verifies that the correct event is returned.
-     */
-    @Test
-    fun `test filtering events by type, radius, start date, and end date`() {
-        val eventSearchParams = EventSearchParams(
-            type = EventType.CONCERT,
-            startDate = now,
-            endDate = now.plus(2, ChronoUnit.DAYS),
-            radius = radius
-        )
-        val filteredEvents = EventsProcessor.filter(events, eventSearchParams, userGPoint)
-        assertEquals(1, filteredEvents.size)
-        assertEquals("Festival", filteredEvents[0].name)
-    }
-
-    /**
-     * Test filtering events with an invalid date range.
-     * - Verifies that no events are returned if startDate > endDate.
-     */
-    @Test
-    fun `test filtering with invalid date range`() {
-        val eventSearchParams = EventSearchParams(
-            startDate = now.plus(1, ChronoUnit.DAYS),
-            endDate = now.minus(1, ChronoUnit.DAYS),
-            radius = null,
-            type = null
-        )
-        val filteredEvents = EventsProcessor.filter(events, eventSearchParams, null)
-        assertTrue(filteredEvents.isEmpty())
-    }
+    data class Case(val input: List<Event>, val filter: EventSearchParams, val expected: List<Event>)
 }
